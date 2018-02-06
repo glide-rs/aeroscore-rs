411c0aa5b28d6e1c5e95b84e188a813d7c3c2175
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-02-06 20:58:40 +0100

    tests: Adjust test for release time

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -20,13 +20,16 @@ impl olc::Point for Point {
 
 #[test]
 fn it_works() {
+    let release_seconds = 10 * 3600 + 28 * 60 + 05;
+
     let fixes = include_str!("fixtures/2017-08-14-fla-6ng-01.igc")
         .lines()
         .filter(|l| l.starts_with('B'))
         .map(|line| Point(igc::parse_fix(&line)))
+        .filter(|point| point.0.seconds_since_midnight >= release_seconds)
         .collect::<Vec<_>>();
 
     let result = olc::optimize(&fixes).unwrap();
 
-    assert_approx_eq!(result.distance, 504.023, 0.01);
+    assert_approx_eq!(result.distance, 501.3, 0.1);
 }
