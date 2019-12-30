97503fe22c2a9bcf8458e7fac415378b6e4d86a6
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-30 18:55:53 +0100

    examples/cli: Output result points too

--- examples/cli.rs
+++ examples/cli.rs
@@ -15,6 +15,25 @@ struct Point {
     altitude: i16,
 }
 
+impl std::fmt::Debug for Point {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let lon_min = (self.longitude - self.longitude.floor()) * 60.;
+        let lat_min = (self.latitude - self.latitude.floor()) * 60.;
+        write!(
+            f,
+            "{:02}:{:02}:{:02}  {:03.0}°{:06.3}E  {:03.0}°{:06.3}N  {:5.0}m",
+            self.time.hours,
+            self.time.minutes,
+            self.time.seconds,
+            self.longitude.floor(),
+            lon_min,
+            self.latitude.floor(),
+            lat_min,
+            self.altitude
+        )
+    }
+}
+
 impl aeroscore::Point for Point {
     fn latitude(&self) -> f64 {
         self.latitude
@@ -55,8 +74,19 @@ fn analyze(path: &str) {
             }))
         .collect::<Vec<_>>();
 
+    println!("num points: {}", fixes.len());
+    println!();
+
     let result = olc::optimize(&fixes).unwrap();
 
+    println!("{:5}:  {:?}", result.point_list[0], fixes[result.point_list[0]]);
+    println!("{:5}:  {:?}", result.point_list[1], fixes[result.point_list[1]]);
+    println!("{:5}:  {:?}", result.point_list[2], fixes[result.point_list[2]]);
+    println!("{:5}:  {:?}", result.point_list[3], fixes[result.point_list[3]]);
+    println!("{:5}:  {:?}", result.point_list[4], fixes[result.point_list[4]]);
+    println!("{:5}:  {:?}", result.point_list[5], fixes[result.point_list[5]]);
+    println!("{:5}:  {:?}", result.point_list[6], fixes[result.point_list[6]]);
+    println!();
     println!("distance: {:.2} km", result.distance);
 }
 
