f8b9b2acde89165e845a2ab58d8ceeaf3cd5f96d
Tobias Bieniek <tobias.bieniek@gmail.com>
2019-12-31 14:14:54 +0100

    haversine: Extract variables to reduce `sin()` calls

--- src/haversine.rs
+++ src/haversine.rs
@@ -7,10 +7,11 @@ pub fn haversine_distance(fix1: &dyn Point, fix2: &dyn Point) -> f64 {
     let phi2 = fix2.latitude().to_radians();
     let delta_phi = (fix2.latitude() - fix1.latitude()).to_radians();
     let delta_rho = (fix2.longitude() - fix1.longitude()).to_radians();
+    let delta_phi_half_sin = (delta_phi / 2.).sin();
+    let delta_rho_half_sin = (delta_rho / 2.).sin();
 
-    let a = (delta_phi / 2.).sin() * (delta_phi / 2.).sin() +
-        phi1.cos() * phi2.cos() *
-            (delta_rho / 2.).sin() * (delta_rho / 2.).sin();
+    let a = delta_phi_half_sin * delta_phi_half_sin +
+        phi1.cos() * phi2.cos() * delta_rho_half_sin * delta_rho_half_sin;
 
     let c = 2. * a.sqrt().atan2((1. - a).sqrt());
 
