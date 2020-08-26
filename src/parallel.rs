a76cd55331a7adeca0538566af6d08fe34aa40dd
Moritz Althaus <moritzalthaus@gmx.de>
2020-08-26 09:58:07 +0200

    Added whitespace that was previously removed

--- src/parallel.rs
+++ src/parallel.rs
@@ -4,6 +4,7 @@ cfg_if! {
     if #[cfg(feature = "rayon")] {
         use rayon::slice;
         pub use rayon::prelude::*;
+
         pub fn opt_par_iter<T: Sync>(x: &[T]) -> slice::Iter<T> {
             x.par_iter()
         }
